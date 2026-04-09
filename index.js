57f25047dab09c85b6dc67c0628de1f308ddb80d
poomichopoom-p <Poomicho.p@gmail.com>
2026-04-09 13:20:15 +0700

    Edit-Index

---
+++ index.js
@@ -0,0 +1,9 @@
+const ConcertTicket = require ("./ConcertTicket")
+const SeatindTicket = require ("./SeatindTicket")
+const StadiumG = require ("./StadiumG")
+const StandingTicket = require ("./StandingTicket")
+const VipTicket = require ("./VipTicket")
+
+
+const seat65 = new seat65(VipTicket,150000,kiss)
+
